5f6953d15870cfee89bbdbdf9ef69acb7bc1a379
Irina Kats <irina8kats@gmail.com>
2024-04-07 10:37:37 -0400

    add input number

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,66 +1,41 @@
 <template>
   <div>
     <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+
+    <h2>Input number with validation</h2>
+    <base-input-number></base-input-number>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  PropType,
-  computed,
-  ref,
-  toRef,
-  Ref,
-} from 'vue';
-import { Todo, Meta } from './models';
+import { defineComponent, ref } from 'vue';
+import BaseInputNumber from './input/number/base-input-number.vue';
 
 function useClickCount() {
   const clickCount = ref(0);
   function increment() {
-    clickCount.value += 1
+    clickCount.value += 1;
     return clickCount.value;
   }
 
   return { clickCount, increment };
 }
 
-function useDisplayTodo(todos: Ref<Todo[]>) {
-  const todoCount = computed(() => todos.value.length);
-  return { todoCount };
-}
-
 export default defineComponent({
   name: 'ExampleComponent',
-
+  components: { BaseInputNumber },
   props: {
     title: {
       type: String,
-      required: true
-    },
-    todos: {
-      type: Array as PropType<Todo[]>,
-      default: () => []
-    },
-    meta: {
-      type: Object as PropType<Meta>,
-      required: true
+      required: true,
     },
     active: {
-      type: Boolean
-    }
+      type: Boolean,
+    },
   },
 
-  setup (props) {
-    return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'todos')) };
-  }
+  setup() {
+    return { ...useClickCount() };
+  },
 });
 </script>
